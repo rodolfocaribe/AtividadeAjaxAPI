c48ee1e3e8abe8523123d6d3d16b249f631a18b2
Rodolfo C Ribeiro <rodolfo.ribeiro@estudante.iftm.edu.br>
2023-03-22 19:46:46 -0300

    Resolvendo exibição dos itens usando display-flex

--- main.js
+++ main.js
@@ -7,48 +7,25 @@
         .then(data => {
             data.results.slice(0, 10).forEach(({poster_path, original_title, release_date, overview, vote_average}) => {
                 const movieDiv = document.createElement("div");
+                movieDiv.classList.add("filme");
                 const posterPath = document.createElement("img");
                 posterPath.src = `https://image.tmdb.org/t/p/w500${poster_path}`;
                 const originalTitle = document.createElement("p");
-                originalTitle.textContent = `Nome original: ${original_title}`;
+                originalTitle.textContent = `Nome: ${original_title}`;
                 const releaseDate = document.createElement("p");
                 releaseDate.textContent = `Data de Lançamento: ${release_date}`;
-                const overviews = document.createElement("p");
-                overviews.textContent = `Descrição: ${overview}`;
+                // const overviews = document.createElement("p");
+                // overviews.textContent = `Descrição: ${overview}`;
                 const voteAverage = document.createElement("p");
                 voteAverage.textContent = `Avaliação média: ${vote_average}`;
 
                 movieDiv.appendChild(posterPath);
                 movieDiv.appendChild(originalTitle);
                 movieDiv.appendChild(releaseDate);
-                movieDiv.appendChild(overviews);
+                // movieDiv.appendChild(overviews);
                 movieDiv.appendChild(voteAverage);
                 moviesList.appendChild(movieDiv);
             });
         });
 })(jQuery);
-$(".movies-list").slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 3
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-            }
-        }
-    ]
-})(jQuery);
+
